2b5ae5f6d1b0dbdfd061e4bc84aaf41943161437
965217357@qq.com <965217357@qq.com>
2022-05-15 16:53:22 +0800

    添加插件注释

--- src/main/java/com/wjw/project/intercaptor/SqlLogPlugin.java
+++ src/main/java/com/wjw/project/intercaptor/SqlLogPlugin.java
@@ -13,10 +13,16 @@ import java.util.Properties;
  * @title: SqlLogPlugin
  * @date 2022/5/12 16:28
  */
+// 注解配置需要拦截的类以及方法
 @Intercepts({
         @Signature(type = StatementHandler.class, method = "query", args = {Statement.class, ResultHandler.class})
 })
+// 实现Interceptor接口
 public class SqlLogPlugin implements Interceptor {
+
+    /**
+     * 具体的拦截逻辑
+     */
     @Override
     public Object intercept(Invocation invocation) throws Throwable {
         long begin = System.currentTimeMillis();
@@ -28,14 +34,23 @@ public class SqlLogPlugin implements Interceptor {
         }
     }
 
-    @Override
+    /**
+     * 判断是否需要进行代理
+     * 此方法有默认实现，一般无需重写
+     */
+    /*@Override
     public Object plugin(Object target) {
         return Plugin.wrap(target, this);
-    }
+    }*/
 
+    /**
+     * 自定义参数
+     */
     @Override
     public void setProperties(Properties properties) {
         // 这是xml中配置的参数
-        System.out.println(properties.propertyNames());
+        properties.forEach((k, v) -> {
+            System.out.printf("SqlLogPlugin---key:%s, value:%s%n", k, v);
+        });
     }
 }
